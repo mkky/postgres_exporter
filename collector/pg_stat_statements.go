453664a661be2218894b4d39704e3c0968302ccf
Mikhail Korepanov <mikhail-korepanov@yandex.ru>
2025-01-24 19:50:37 +0400

    Renamed columns in collector/pg_stat_statements.go: blk_read_time → shared_blk_read_time, blk_write_time → shared_blk_write_time

--- collector/pg_stat_statements.go
+++ collector/pg_stat_statements.go
@@ -99,8 +99,8 @@ var (
 		pg_stat_statements.calls as calls_total,
 		pg_stat_statements.total_exec_time / 1000.0 as seconds_total,
 		pg_stat_statements.rows as rows_total,
-		pg_stat_statements.blk_read_time / 1000.0 as block_read_seconds_total,
-		pg_stat_statements.blk_write_time / 1000.0 as block_write_seconds_total
+		pg_stat_statements.shared_blk_read_time / 1000.0 as block_read_seconds_total,
+		pg_stat_statements.shared_blk_write_time / 1000.0 as block_write_seconds_total
 		FROM pg_stat_statements
 	JOIN pg_database
 		ON pg_database.oid = pg_stat_statements.dbid
